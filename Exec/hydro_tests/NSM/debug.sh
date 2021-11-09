116839d78734af2f42edd88fa60f76f0c12d7203
Kaley Brauer <kaleybrauer@gmail.com>
2021-11-09 14:24:48 -0800

    added NSM code, based on Sedov example

---
+++ Exec/hydro_tests/NSM/debug.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH --qos=debug
+#SBATCH --time=00:30:00
+#SBATCH --constraint=haswell
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=2
+#SBATCH --mail-user=kbrauer@lbl.gov
+#SBATCH --mail-type=ALL
+
+export MPICH_MAX_THREAD_SAFETY=multiple # NEED THIS OR WILL FAIL
+
+# cd $SLURM_SUBMIT_DIR
+# export OMP_PLACES=cores
+# export OMP_PROC_BIND=true
+# export OMP_NUM_THREADS=4
+
+srun --cpu_bind=cores ./Castro2d.gnu.haswell.DEBUG.MPI.ex inputs.2d.sph2
+# srun --cpu_bind=cores ./Castro3d.gnu.haswell.DEBUG.MPI.ex inputs.3d.sph
